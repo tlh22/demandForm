cf64636607960b03f69227015a2826c004b2c5dd
tlh22 <thancock@mhtc.co.uk>
2021-12-02 22:24:48 +0100

    Update 0011_output_count.sql

--- DATAMODEL/DemandSetup/0011_output_count.sql
+++ DATAMODEL/DemandSetup/0011_output_count.sql
@@ -35,6 +35,10 @@ UPDATE demand."Demand_Merged"
 SET nspaces = NULL
 WHERE nspaces = '';
 
+UPDATE demand."Demand_Merged"
+SET sbays = NULL
+WHERE sbays = '';
+
 -- Check
 SELECT "SurveyID",
     SUM(COALESCE("ncars"::float, 0.0) +
@@ -118,7 +122,7 @@ d."SurveyID", s."SurveyDay" As "Survey Day", s."BeatStartTime" || '-' || s."Beat
        COALESCE("nmcls"::float, 0) AS "Nr MCLs", COALESCE("nogvs"::float, 0) AS "Nr OGVs", COALESCE("nbuses"::float, 0) AS "Nr Buses",
        COALESCE("nspaces"::float, 0) AS "Nr Spaces",
        COALESCE(d."sbays"::integer, 0) AS "Bays Suspended", d."snotes" AS "Suspension Notes", "Demand" As "Demand",
-             d."nnotes" AS "Surveyor Notes"
+             d."nnotes" AS "Surveyor Notes",
         su."RestrictionTypeID", su."Capacity"
 
 FROM --"SYL_AllowableTimePeriods" syls,
